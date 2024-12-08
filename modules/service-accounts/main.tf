ef780073b1b85097e36b1a7d3ccca761f93c82b8
Rotimi Abiola <rtmabiola@gmail.com>
2024-12-08 10:55:56 +0100

    `Update GitHub Actions workflows and Terraform module to use project ID instead of service account email`

--- modules/service-accounts/main.tf
+++ modules/service-accounts/main.tf
@@ -38,8 +38,10 @@ resource "google_iam_workload_identity_pool_provider" "github_provider" {
   
   attribute_mapping = {
     "google.subject"       = "assertion.sub"
-    "attribute.actor"      = "assertion.actor"
     "attribute.repository" = "assertion.repository"
+    "attribute.ref"        = "assertion.ref"
+    "attribute.workflow"   = "assertion.workflow"
+    "attribute.actor"      = "assertion.actor"
   }
   
   oidc {
